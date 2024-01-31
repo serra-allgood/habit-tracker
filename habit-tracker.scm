e7550e98d913180fb042159807b2131baea0c38a
Serra Allgood <serra@allgood.dev>
2024-01-31 03:37:20 -0500

    Improve conditional execution and make output append to file
    Signed-off-by: Serra Allgood <serra@allgood.dev>

--- habit-tracker.scm
+++ habit-tracker.scm
@@ -7,6 +7,11 @@
               (srfi srfi-19))
 
 (define habit-tracker-dir (string-append (passwd:dir (getpw (getlogin))) file-name-separator-string ".habit-tracker"))
+(define make-habit-dir (lambda ()
+  (begin
+    (mkdir habit-tracker-dir)
+    (display (string-append "Created habit-tracker directory at " habit-tracker-dir))
+    (newline))))
 
 (define (main args)
   (let* (
@@ -17,33 +22,24 @@
     (options (getopt-long args option-spec))
     (help-wanted (option-ref options 'help #f))
     (add-habit (option-ref options 'add #f))
-  )
-  (if help-wanted
-    (begin
+    (display-help-message (lambda () (begin
       (display "\
 habit-tracker [options]
 -a, --add     Add a habit entry
 -h, --help    Display this help message")
-      (newline))
-    (begin
-      (if (not (file-exists? habit-tracker-dir))
-        (begin
-          (mkdir habit-tracker-dir)
-          (display (string-append "Created habit-tracker directory at " habit-tracker-dir))
-          (newline)))
-      (if (not (string-null? add-habit))
-        (let* (
-          (now (date->string (current-date) "~1"))
-          (file-name (string-append habit-tracker-dir file-name-separator-string now))
-        )
-        (begin
-          (call-with-output-file file-name
-            (lambda (port) (put-string port (string-append add-habit "\n")))
-          ))
-          (display (string-append add-habit " has been logged for " now))
-        )
-      )
-      (newline)
-    )
+      (newline))))
   )
-))
+  (cond
+    (help-wanted (display-help-message))
+    ((not (string-null? add-habit))
+      (begin
+        (unless (file-exists? habit-tracker-dir) (make-habit-dir))
+        (let* ((now (date->string (current-date) "~1"))
+              (file-name (string-append habit-tracker-dir file-name-separator-string now))
+              (port (open-file file-name "a")))
+        (begin
+          (display add-habit port)
+          (newline port)
+          (close port))
+          (display (string-append add-habit " has been logged for " now))))
+        (newline)))))
